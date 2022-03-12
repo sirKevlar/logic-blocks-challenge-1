270f0fdd34a23501f2dbbbc5e0df5dadf48fbcd1
Kev Morel <kev.morel@northcoders.com>
2022-03-12 21:04:03 +0000

    removes logs

--- idTimesMinutes.js
+++ idTimesMinutes.js
@@ -1,9 +1,13 @@
-const {data} = require('./data');
+const { data } = require('./data');
 const findBestMinutesAndOfficer = require('./findBestMinutesAndOfficer');
 
 const idTimesMinutes = (securityData) => {
   const bestTimes = findBestMinutesAndOfficer(securityData);
   const minTimesId = bestTimes.sleepiestTimes.map((minute) => {
-    return minute * bestTimes.officer  
-  })
+    return minute * bestTimes.officer;
+  });
+  return minTimesId;
 };
+
+// const result = idTimesMinutes(data);
+// console.log(result);
